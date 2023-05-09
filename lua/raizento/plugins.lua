af8517fde3dd625b289290694827d6bbd67bd933
raizento <it.alexander.molitor@gmail.com>
2023-05-09 22:35:08 +0200

    Added hardhacker theme; disabled due to issues

--- lua/raizento/plugins.lua
+++ lua/raizento/plugins.lua
@@ -49,6 +49,9 @@ return packer.startup(function(use)
     use "navarasu/onedark.nvim"
     use { "catppuccin/nvim", as = "catppuccin" }
     use "tyrannicaltoucan/vim-quantum"
+    use "hardhackerlabs/theme-vim"
+    -- Currently having some issues; check later
+    -- use {'hardhackerlabs/theme-vim', as = 'hardhacker'}
 
     -- cmp plugins
     use "hrsh7th/nvim-cmp" -- completion plugin

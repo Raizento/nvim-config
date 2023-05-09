0afb51888a866082ec5c33f5a7666d15ca2f3511
raizento <it.alexander.molitor@gmail.com>
2023-05-09 22:44:25 +0200

    Added Tokyonight colorscheme

--- lua/raizento/plugins.lua
+++ lua/raizento/plugins.lua
@@ -49,8 +49,8 @@ return packer.startup(function(use)
     use "navarasu/onedark.nvim"
     use { "catppuccin/nvim", as = "catppuccin" }
     use "tyrannicaltoucan/vim-quantum"
-    -- Currently having some issues; check later
-    use {'hardhackerlabs/theme-vim', as = 'hardhacker' }
+    use { "hardhackerlabs/theme-vim", as = "hardhacker" }
+    use "folke/tokyonight.nvim"
 
     -- cmp plugins
     use "hrsh7th/nvim-cmp" -- completion plugin

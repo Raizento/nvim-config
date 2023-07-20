12965c4fadd3a3bc354c58bd97dc69ca9e95683c
raizento <it.alexander.molitor@gmail.com>
2023-07-20 18:20:35 +0200

    Added super tab configuration for cmp

--- lua/raizento/cmp.lua
+++ lua/raizento/cmp.lua
@@ -9,6 +9,11 @@ end
 local luasnip = require "luasnip"
 local cmp = require "cmp"
 
+local check_backspace = function()
+    local col = vim.fn.col "." - 1
+    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
+
 cmp.setup({
     snippet = {
         expand = function(args)
@@ -24,6 +29,34 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm(), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- Super-Tab setup
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif check_backspace() then
+                fallback()
+            else
+                fallback()
+            end
+        end, {
+            'i',
+            's',
+        }),
+
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            elseif check_backspace() then
+                fallback()
+            end
+        end, {
+            'i',
+            's',
+        }),
     },
 
     sources = cmp.config.sources({

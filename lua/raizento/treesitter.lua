8a105b367df5b949add6c19dc926c9ecd23cd72a
raizento <it.alexander.molitor@gmail.com>
2023-07-21 19:55:19 +0200

    Treesitter now uses git for installing

--- lua/raizento/treesitter.lua
+++ lua/raizento/treesitter.lua
@@ -1,3 +1,4 @@
+require("nvim-treesitter.install").prefer_git = true
 require "nvim-treesitter.configs".setup {
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "gitignore", "json", "yaml", "toml", "regex", "groovy", "java" },
 

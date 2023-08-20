2dc811f04f3489416cb38b85dd7ce60f284398e4
raizento <it.alexander.molitor@gmail.com>
2023-08-20 12:06:56 +0200

    Added ctags capabilities to cmp

--- lua/raizento/lazy.lua
+++ lua/raizento/lazy.lua
@@ -21,9 +21,7 @@ require("lazy").setup({
   "hrsh7th/cmp-path",
   "hrsh7th/cmp-cmdline",
   "hrsh7th/nvim-cmp",
+  "delphinus/cmp-ctags",
   "L3MON4D3/LuaSnip",
   "saadparwaiz1/cmp_luasnip",
 })
-
-
-

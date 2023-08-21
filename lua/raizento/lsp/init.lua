3ef715440d8996465ac76c6ef45e367b68457d3a
raizento <it.alexander.molitor@gmail.com>
2023-08-21 14:28:12 +0200

    Added LSP keybindings

--- lua/raizento/lsp/init.lua
+++ lua/raizento/lsp/init.lua
@@ -37,10 +37,46 @@ mason_lsp.setup_handlers(handlers.handlers)
 
 whichkey.register({
   d = {
-    name = "diagnostic",
+    name = "Diagnostic",
     e = { vim.diagnostic.open_float, "Open float", },
     p = { vim.diagnostic.goto_prev, "Previous", },
     n = { vim.diagnostic.goto_next, "Next", },
     q = { vim.diagnostic.setloclist, "Open loclist", },
   },
 }, { prefix = "<Leader>" })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+    
+    whichkey.register({
+      K = { vim.lsp.buf.hover, "Hover" },
+      ["<C-k>"] = { vim.lsp.buf.signature_help, "Signature help" },
+    }, { buffer = ev.buf})
+
+    whichkey.register({
+      l = {
+        name = "LSP",
+        w = {
+          name = "Workspace",
+          a = { vim.lsp.buf.add_workspace_folder, "Add folder", },
+          r = { vim.lsp.buf.remove_workspace_folder, "Remove folder", },
+          l = { function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, "List folders", },
+        },
+        a = { vim.lsp.buf.code_action, "Actions", mode = { "n", "v", } },
+        f = { function() vim.lsp.buf.format { async = true } end, "Format" },
+      }
+    }, { buffer = ev.buf, prefix = "<Leader>" })
+
+    whichkey.register({
+      j = {
+        name = "jump to",
+        D = { vim.lsp.buf.declaration, "Declaration" },
+        d = { vim.lsp.buf.definition, "Definition" },
+        i = { vim.lsp.buf.implementation, "Implementation" },
+        r = { vim.lsp.buf.references, "References" },
+        t = { vim.lsp.buf.type_definition, "Type definition" },
+    } { buffer = ev.buf, prefix = "<Leader>" })
+  end
+})

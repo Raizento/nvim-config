85327af13d04845ff122ad6166fe36bc98bc02bc
raizento <it.alexander.molitor@gmail.com>
2024-01-12 23:25:36 +0100

    Fixed descriptions for telescope commands

--- lua/raizento/plugins/telescope.lua
+++ lua/raizento/plugins/telescope.lua
@@ -5,20 +5,20 @@ local M = {
     "nvim-telescope/telescope-file-browser.nvim",
   },
   keys = {
-    { "<Leader>ff", "<CMD>Telescope find_files<CR>", { desc = "find files" } },
-    { "<Leader>fg", "<CMD>Telescope live_grep<CR>", { desc = "live grep" } },
-    { "<Leader>fb", "<CMD>Telescope buffers<CR>", { desc = "buffers" } },
-    { "<Leader>fh", "<CMD>Telescope help_tags<CR>", { desc = "help tags" } },
-    { "<Leader>fc", "<CMD>Telescope commands<CR>", { desc = "commands" } },
+    { "<Leader>ff", "<CMD>Telescope find_files<CR>", desc = "find files" },
+    { "<Leader>fg", "<CMD>Telescope live_grep<CR>", desc = "live grep" },
+    { "<Leader>fb", "<CMD>Telescope buffers<CR>", desc = "buffers" },
+    { "<Leader>fh", "<CMD>Telescope help_tags<CR>", desc = "help tags" },
+    { "<Leader>fc", "<CMD>Telescope commands<CR>", desc = "commands" },
     {
       "<Leader>fl",
       "<CMD>lua vim.diagnostic.setloclist({ open = false })<CR><CMD>Telescope loclist<CR>",
-      { desc = "loclist" },
+      desc = "loclist"
     },
-    { "<Leader>ft", "<CMD>Telescope tags<CR>", { desc = "tags" } },
-    { "<Leader>fj", "<CMD>Telescope jumplist<CR>", { desc = "jumps" } },
-    { "<Leader>fj", "<CMD>Telescope search_history<CR>", { desc = "search history" } },
-    { "<Leader>B", "<CMD>Telescope file_browser<CR>", { desc = "telescope file browser" } },
+    { "<Leader>ft", "<CMD>Telescope tags<CR>", desc = "tags" },
+    { "<Leader>fj", "<CMD>Telescope jumplist<CR>", desc = "jumps" },
+    { "<Leader>fj", "<CMD>Telescope search_history<CR>", desc = "search history" },
+    { "<Leader>B", "<CMD>Telescope file_browser<CR>", desc = "telescope file browser" },
   },
 }
 

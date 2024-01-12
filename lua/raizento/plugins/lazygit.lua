d145b28614f703debbed070029e4fd5081ac979e
raizento <it.alexander.molitor@gmail.com>
2024-01-12 23:52:25 +0100

    Fixed map desc for lazygit

--- lua/raizento/plugins/lazygit.lua
+++ lua/raizento/plugins/lazygit.lua
@@ -2,7 +2,7 @@ local M = {
   "kdheepak/lazygit.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   keys = {
-    { "<Leader>gl", "<CMD>LazyGit<CR>", { desc = "lazygit" } },
+    { "<Leader>gl", "<CMD>LazyGit<CR>", desc = "lazygit" },
   },
 }
 

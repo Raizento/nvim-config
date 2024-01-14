98849819e5b4d3337338da5788ca6371fe3ab855
Raizento <it.alexander.molitor@gmail.com>
2024-01-14 21:24:55 +0100

    Now using on_attach call of lsp servers instead of autocmd

--- lua/raizento/lsp/config/lua_ls.lua
+++ lua/raizento/lsp/config/lua_ls.lua
@@ -1,10 +1,11 @@
 local M = {}
 
-M.config = function(capabilities)
+M.config = function(capabilities, on_attach)
   return function()
     require("neodev").setup({})
     require("lspconfig").lua_ls.setup({
       capabilities = capabilities,
+      on_attach = on_attach,
     })
   end
 end
--- lua/raizento/lsp/config/rust_analyzer.lua
+++ lua/raizento/lsp/config/rust_analyzer.lua
@@ -1,8 +1,12 @@
 local M = {}
 
-M.config = function(capabilities)
+M.config = function(capabilities, on_attach)
   return function()
-    require("rust-tools").setup({})
+    require("rust-tools").setup({
+      server = {
+        on_attach = on_attach,
+      },
+    })
   end
 end
 

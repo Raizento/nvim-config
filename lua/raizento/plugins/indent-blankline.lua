e44a003b062bc45d14b0c999287f1fd886d461b3
Raizento <it.alexander.molitor@gmail.com>
2024-01-17 16:15:43 +0100

    Formatting

--- lua/raizento/plugins/indent-blankline.lua
+++ lua/raizento/plugins/indent-blankline.lua
@@ -1,18 +1,18 @@
 local M = {
-    "lukas-reineke/indent-blankline.nvim",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter"
-    },
-    main = "ibl",
+  "lukas-reineke/indent-blankline.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  main = "ibl",
 }
 
 M.config = function()
-    require("ibl").setup({
-        scope = {
-            show_start = false,
-            show_end = false,
-        }
-    })
+  require("ibl").setup({
+    scope = {
+      show_start = false,
+      show_end = false,
+    },
+  })
 end
 
 return M
--- lua/raizento/plugins/ufo.lua
+++ lua/raizento/plugins/ufo.lua
@@ -1,21 +1,21 @@
 local M = {
-    "kevinhwang91/nvim-ufo",
-    dependencies = {
-        "kevinhwang91/promise-async",
-        "nvim-treesitter/nvim-treesitter",
-    },
-    run = ":TSUpdate",
+  "kevinhwang91/nvim-ufo",
+  dependencies = {
+    "kevinhwang91/promise-async",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  run = ":TSUpdate",
 }
 
 M.config = function()
-    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+  vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+  vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-    require("ufo").setup({
-        provider_selector = function(bufnr, filetype, buftype)
-            return { "treesitter" , "indent" }
-        end
-    })
+  require("ufo").setup({
+    provider_selector = function(bufnr, filetype, buftype)
+      return { "treesitter", "indent" }
+    end,
+  })
 end
 
 return M

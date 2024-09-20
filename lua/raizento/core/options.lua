4694236ce3a9c018fe2348084695d7db6307286f
Raizento <it.alexander.molitor@gmail.com>
2024-09-20 21:20:50 +0200

    Replaced .opt for .o

--- lua/raizento/core/options.lua
+++ lua/raizento/core/options.lua
@@ -1,54 +1,54 @@
 -- Vim options
 vim.g.mapleader = " "
-vim.g.maplocalleader = [[\]] 
+vim.g.maplocalleader = [[\]]
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.numberwidth = 4
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.numberwidth = 4
 
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.cindent = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
-vim.opt.smarttab = true
+vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.cindent = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.smarttab = true
 
-vim.opt.mouse = "a"
+vim.o.mouse = "a"
 
-vim.opt.foldcolumn = "6"
+vim.o.foldcolumn = "6"
 
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
+vim.o.incsearch = true
+vim.o.ignorecase = true
 
-vim.opt.backspace = "indent,eol,start"
-vim.opt.matchpairs = "(:),{:},[:],<:>"
+vim.o.backspace = "indent,eol,start"
+vim.o.matchpairs = "(:),{:},[:],<:>"
 
-vim.opt.magic = true -- plugins may break if this option is turned off
+vim.o.magic = true -- plugins may break if this option is turned off
 
-vim.opt.ruler = true
+vim.o.ruler = true
 
-vim.opt.undofile = true
+vim.o.undofile = true
 
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.o.splitright = true
+vim.o.splitbelow = true
 
-vim.opt.scrolloff = 4
-vim.opt.sidescrolloff = 8
+vim.o.scrolloff = 4
+vim.o.sidescrolloff = 8
 
-vim.opt.wrap = false
+vim.o.wrap = false
 
-vim.opt.termguicolors = true
+vim.o.termguicolors = true
 
-vim.opt.title = true
+vim.o.title = true
 
-vim.opt.showmode = false -- Lualine takes care of indicating the mode
+vim.o.showmode = false -- Lualine takes care of indicating the mode
 
-vim.opt.foldlevelstart = 99
+vim.o.foldlevelstart = 99
 
-vim.opt.updatetime = 100 -- TreeSitter updates definition highlighting every {updatetime} ms
+vim.o.updatetime = 100 -- TreeSitter updates definition highlighting every {updatetime} ms
 
-vim.opt.virtualedit = "block"
+vim.o.virtualedit = "block"
 
-vim.opt.inccommand = "split"
-vim.opt.cmdwinheight = 16
+vim.o.inccommand = "split"
+vim.o.cmdwinheight = 16

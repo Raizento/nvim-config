a67892aa04feb14eca5f1e2b27488bba71b43893
Raizento <it.alexander.molitor@gmail.com>
2024-09-23 16:32:57 +0200

    Removed lazygit telescope extension

--- lua/raizento/plugins/lazygit.lua
+++ lua/raizento/plugins/lazygit.lua
@@ -1,13 +1,9 @@
 local M = {
   "kdheepak/lazygit.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+  dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<Leader>gl", "<CMD>LazyGit<CR>", desc = "lazygit" },
   },
 }
-
-M.config = function()
-  require("telescope").load_extension("lazygit")
-end
 
 return M

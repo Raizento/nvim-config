5b3b8d932b48bcf121307193542952a5791702cd
Raizento <51487469+Raizento@users.noreply.github.com>
2024-10-06 22:29:15 +0200

    5 replace rust tools with rustaceanvim (#13)
    * Disable setup for rust_analyzer
    
    * Added rustacean

--- lua/raizento/init.lua
+++ lua/raizento/init.lua
@@ -18,8 +18,10 @@ spec("raizento.plugins.oil")
 spec("raizento.plugins.transparent")
 spec("raizento.plugins.which-key")
 
+-- TODO maybe create another table for these lsp plugins
 spec("raizento.lsp.lazydev")
 spec("raizento.lsp.nvim-java")
+spec("raizento.lsp.rustacean")
 
 spec("raizento.lsp.lspconfig")
 spec("raizento.lsp.mason")

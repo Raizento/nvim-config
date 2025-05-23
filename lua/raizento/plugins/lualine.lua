131f180f423dcf604d21d6b8cbad148a76aabb8e
Raizento <it.alexander.molitor@gmail.com>
2025-05-24 01:12:12 +0200

    Changed branch name shortening a bit

--- lua/raizento/plugins/lualine.lua
+++ lua/raizento/plugins/lualine.lua
@@ -3,20 +3,26 @@ local M = {
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
 
+M.shorten_branch_name = function(text, context)
+  if text:len() > 40 then
+    return text:sub(1,37) .. "..."
+  else
+    return text
+  end
+end
+
 M.opts = {
   sections = {
     lualine_b = {
-      'branch',
-      fmt = M.shorten_branch_name,
+      {
+        'branch',
+        fmt = M.shorten_branch_name,
+      },
     },
   },
   options = {
       globalstatus = true,
   },
 }
-
-M.shorten_branch_name = function(text, context)
-  return text:sub(1,40)
-end
 
 return M

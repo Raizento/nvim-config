2136be1d1641eb74873f855cf808d200ebda66ad
Raizento <it.alexander.molitor@gmail.com>
2025-06-15 17:14:35 +0200

    Make test-shim work with CI

--- tests/test-shim.sh
+++ tests/test-shim.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-nvim --cmd 'set loadplugins' --cmd 'lua vim.g.istest = true' -u ~/.config/nvim/init.lua -l $@
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+nvim --cmd 'set loadplugins' --cmd 'lua vim.g.istest = true' -u $XDG_CONFIG_HOME/nvim/init.lua -l $@
 exit_code=$?
 
 exit $exit_code

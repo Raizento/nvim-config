387eabb3ddf9574f521a3ab568a2f0a1f799af5c
Raizento <it.alexander.molitor@gmail.com>
2026-03-27 22:41:24 +0100

    Solve spring boot lsp sometimes not being able to start
    This was caused by mason sometimes loading after the spring boot lsp, but the spring boot lsp depends on Mason

--- lua/raizento/lsp/plugins/java.lua
+++ lua/raizento/lsp/plugins/java.lua
@@ -4,6 +4,7 @@ local Plugin = require("raizento.plugins.plugin")
 local M = {
   dependencies = {
     { url = "https://github.com/mfussenegger/nvim-jdtls", name = "jdtls" },
+    "https://github.com/mason-org/mason.nvim",
   },
   url = "https://github.com/JavaHello/spring-boot.nvim",
   name = "spring_boot",
